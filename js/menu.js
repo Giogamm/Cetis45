2379c95e5e1d490e7fedf671e2bce433ac5acb27
Giogamm <giovanymemije@gmail.com>
2024-06-25 19:57:04 -0600

    tal vez

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const navLinks = document.getElementById("nav-links");
+  const overlay = document.getElementById("overlay");
+
+  menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+
+  overlay.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+
+  navLinks.addEventListener("click", (event) => {
+    if (event.target.tagName === "A") {
+      navLinks.classList.remove("active");
+      overlay.classList.remove("active");
+    }
+  });
+});
